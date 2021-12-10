1ac07a46832941d9b4886b0277b30c128ff3845d
Ricardo <rgallego@iesrafaelalberti.es>
2021-12-10 19:37:16 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
+package un1.eje5_1
+
+data class Ingrediente(val nombre: String, val cantidad: Double, val unidad: String)
+data class Receta(val nombre: String, val ingredientes: Set<Ingrediente>, val pasos: Map<Int, String>)
+
+
 fun main() {
+    var unaReceta = Receta(
+        "Arroz con Leche",
+        setOf(
+            Ingrediente("Arroz", 1000.0, "gr"),
+            Ingrediente("Leche", 1.0, "l"),
+            Ingrediente("Canela en rama", 1.0, "palo")
+        ),
+        mapOf(
+            1 to "Calentar la leche",
+            2 to "Echar el arroz",
+            3 to "Remover frecuentemente",
+            4 to "Apagar el fuego cuando el arroz este blandito con el corazon duro"
+        )
+    )
+    println("$unaReceta")
 
 }

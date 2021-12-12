f3aed1066703e04a62b9ec0c5692f326be6166f6
Ricardo <rgallego@iesrafaelalberti.es>
2021-12-12 18:10:52 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,18 @@ package un1.eje5_1
 data class Ingrediente(val nombre: String, val cantidad: Double, val unidad: String)
 data class Receta(val nombre: String, val ingredientes: Set<Ingrediente>, val pasos: Map<Int, String>)
 
+ class ArmaDeFuego(var nombre: String,var municion:Int,var municionARestar:Int,var tipoDeMunicion:String,var danio:Int,radio:String){
+    init {if (radio.lowercase() != "pequeño" || radio.lowercase() != "amplio") {println("ERROR: Solo puedes introducir los valores Pequeño o Amplio para la variable radio")} }
+
+    fun dispara(municionARestar: Int){municionARestar++}
+    fun recarga(municion: Int) {municion++}
+
+}
+
 
 fun main() {
     var unaReceta = Receta(
-        "Arroz con Leche",
+        "Arrzo con Leche",
         setOf(
             Ingrediente("Arroz", 1000.0, "gr"),
             Ingrediente("Leche", 1.0, "l"),

c5438b9c4c14f2d26f2575a7a565e3e7e6e8fe69
Ricardo <91693885+rgdzalberti@users.noreply.github.com>
2021-12-13 21:33:05 +0100

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,21 @@
+import kotlin.random.Random
 
-open class ArmaDeFuego(nombre: String,municion:Int,municionARestar:Int,tipoDeMunicion:String, danio:Int,radio:String){
-     private val nombre = nombre
-     private var municion = municion
-     set (value) = if (value>0) {field = value} else throw IllegalArgumentException("El valor no puede ser menor de 0")
-     private var municionARestar = municionARestar
-     set (value) = if (value>0) {field = value} else throw IllegalArgumentException("El valor no puede ser menor de 0")
-     private val tipoDeMunicion = tipoDeMunicion
-     private var danio = danio
-     private val radio = radio
+open class ArmaDeFuego(nombre: String, municion:Int, municionARestar:Int, tipoDeMunicion:String, danio:Int, radio:String){
+
+    private val nombre = nombre
+    private var municion = municion
+        set (value) = if (value>0) {field = value} else throw IllegalArgumentException("El valor no puede ser menor de 0")
+    private var municionARestar = municionARestar
+        set (value) = if (value>0) {field = value} else throw IllegalArgumentException("El valor no puede ser menor de 0")
+    private val tipoDeMunicion = tipoDeMunicion
+    private var danio = danio
+    private val radio = radio
 
     init { require(radio.lowercase() == "pequeño" || radio.lowercase() == "amplio"){"La variable radio solo puede ser \"Pequeño\" o \"Amplio\""} }
+
+    override fun toString(): String {
+        return "Esta $nombre tiene $municion de municion, $municionARestar de municion a restar,$tipoDeMunicion es su tipo de munición,$danio de daño y tiene un alcance  $radio"
+    }
 
     fun dispara(){municionARestar++}
     fun recarga(municion: Int) {this.municion = municion}
@@ -29,8 +35,31 @@ open class ArmaDeFuego(nombre: String,municion:Int,municionARestar:Int,tipoDeMun
 
 }
 
-
 fun main() {
+
+    val Thompson = ArmaDeFuego("Thompson",25,1,"Pistola",5,"Pequeño")
+    val Bolt = ArmaDeFuego("Bolt",5,1,"Ligera",60,"Amplio")
+    val Colt45 = ArmaDeFuego("Colt45",13,1,"Pistola",8,"Pequeño")
+
+    val listaArmas = mutableMapOf<Int,ArmaDeFuego>()
+
+    repeat(6){
+        var contador = Random.nextInt(1,3)
+        var i = 0
+
+        when (contador){
+            1 -> listaArmas[i] = Thompson
+            2 -> listaArmas[i] = Bolt
+            3 -> listaArmas[i] = Colt45
+        }
+
+        i++
+    }
+
+    for (i in 0 until listaArmas.size){listaArmas[i]?.dispara();println(listaArmas[i])}
+
+
+
 
 
 }
